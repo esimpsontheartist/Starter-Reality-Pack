8fd3eeaa42269e734cc89cd086bb0827c7a5ecfc
Josh Field <joshfield999@gmail.com>
2021-09-29 10:36:45 +1000

    add client styling

---
+++ StarterClientSkin.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import styles from './Starter.module.scss'
+
+const StarterClientSkin = (props) => {
+  return (
+    <span className={styles.starterSplash}>{"Starter Reality Pack!"}</span>
+  )
+}
+
+export default StarterClientSkin
